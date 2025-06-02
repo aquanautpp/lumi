c86d42681ffd80723d061760f8a54bde8b45030c
aquanautpp <victorpires6744@gmail.com>
2025-06-02 17:52:33 -0300

    Create historico.js

---
+++ utils/historico.js
@@ -0,0 +1,24 @@
+import { salvarMemoria } from './persistencia.js';
+
+function atualizarMemoria(numero, categoria, acertou, respostaUsuario, respostaCorreta) {
+  if (!global.memoriaUsuarios[numero]) {
+    global.memoriaUsuarios[numero] = {
+      nome: null,
+      historico: [],
+      nivelAnterior: 1
+    };
+  }
+
+  global.memoriaUsuarios[numero].historico.push({
+    data: new Date().toISOString(),
+    categoria,
+    respostaUsuario,
+    respostaCorreta,
+    acertou
+  });
+
+  salvarMemoria(); // Persistência após atualização
+}
+
+export { atualizarMemoria };
+feat: adiciona função de atualização de histórico
