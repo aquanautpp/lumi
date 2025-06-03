d1d73c38cc8ecf7a71ff0d1686272b195e41ca5d
aquanautpp <victorpires6744@gmail.com>
2025-06-02 21:06:19 -0300

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -9,4 +9,27 @@ cloudinary.config({
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-export default cloudinary;
+async function uploadPdfToCloudinary(filePath) {
+  return new Promise((resolve, reject) => {
+    cloudinary.uploader.upload(
+      filePath,
+      {
+        resource_type: 'raw', // PDF não é imagem nem vídeo
+        folder: 'lumi-reports',
+        use_filename: true,
+        unique_filename: false
+      },
+      (error, result) => {
+        if (error) {
+          console.error('❌ Erro ao enviar PDF para o Cloudinary:', error);
+          reject(error);
+        } else {
+          console.log(`✅ PDF enviado com sucesso: ${result.secure_url}`);
+          resolve(result.secure_url);
+        }
+      }
+    );
+  });
+}
+
+export { uploadPdfToCloudinary };
