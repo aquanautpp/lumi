274e45467161f56f24bacfd74b1f02371d175170
aquanautpp <victorpires6744@gmail.com>
2025-06-03 23:22:49 -0300

    Update historico.js

--- utils/historico.js
+++ utils/historico.js
@@ -1,22 +1,32 @@
 import { salvarMemoria, memoriaUsuarios } from './memoria.js';
 
-function atualizarMemoria(numero, categoria, acertou, respostaUsuario, respostaCorreta) {
+function atualizarMemoria(numero, categoria, acertou, respostaUsuario, respostaCorreta, enunciado) {
   if (!memoriaUsuarios[numero]) {
     memoriaUsuarios[numero] = {
       nome: null,
       historico: [],
-             nivelAnterior: 1
-             nivelAtual: 1
+      nivelAnterior: 1,
+      nivelAtual: 1,
+      resolvidos: []
     };
   }
 
-  memoriaUsuarios[numero].historico.push({
+  const usuario = memoriaUsuarios[numero];
+  usuario.historico.push({
     data: new Date().toISOString(),
     categoria,
+    enunciado,
     respostaUsuario,
     respostaCorreta,
     acertou
   });
+
+  if (enunciado) {
+    usuario.resolvidos = usuario.resolvidos || [];
+    if (!usuario.resolvidos.includes(enunciado)) {
+      usuario.resolvidos.push(enunciado);
+    }
+  }
 
   salvarMemoria();
 }
