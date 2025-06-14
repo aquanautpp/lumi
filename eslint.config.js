b287da81581f945336f10148836f2df942f40449
aquanautpp <victorpires6744@gmail.com>
2025-06-13 23:40:37 -0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,12 @@
-export default async () => {
-  const importPlugin = (await import('eslint-plugin-import')).default;
-
-  return [
-    {
-      files: ['**/*.js'],
-      ignores: ['node_modules/**'],
-      languageOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-      plugins: { import: importPlugin },
-      rules: {
-        'import/order': ['error', { 'newlines-between': 'always' }],
-        'no-unused-vars': 'warn',
-        'no-console': 'off'
-      }
+export default [
+  {
+    files: ['**/*.js'],
+    ignores: ['node_modules/**'],
+    languageOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+      'no-undef': 'error'
     }
-  ];
-};
+  }
+];
