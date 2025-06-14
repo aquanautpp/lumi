797ec9e00c99c232eb975034402198d60e434f79
aquanautpp <victorpires6744@gmail.com>
2025-06-14 00:56:35 -0300

    Update openaiClient.js

--- utils/openaiClient.js
+++ utils/openaiClient.js
@@ -6,12 +6,10 @@ dotenv.config();
 const openai = new OpenAI();
 
 export async function chatCompletion(messages, options = {}) {
-  const controller = new AbortController();
   const res = await openai.chat.completions.create({
     model: 'gpt-4o-mini',
     messages,
     ...options,
-    abortSignal: controller.signal,
   });
   return res.choices[0].message;
 }
